3af55883be1b45821accdce4177f53871c19e39a
MingyeomKim <67851124+MingyeomKim@users.noreply.github.com>
2023-03-24 16:01:11 +0900

    [hotfix] Zuul, Config 서버 설정 변경

--- msa-architecture-zuul-server/src/main/java/com/personal/zuul/ZuulServerApplication.java
+++ msa-architecture-zuul-server/src/main/java/com/personal/zuul/ZuulServerApplication.java
@@ -10,14 +10,7 @@ import org.springframework.context.annotation.Bean;
 @EnableEurekaClient
 @SpringBootApplication
 public class ZuulServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ZuulServerApplication.class, args);
     }
-
-    @Bean
-    public SimpleFilter simpleFilter(){
-        return new SimpleFilter();
-    }
-
 }
