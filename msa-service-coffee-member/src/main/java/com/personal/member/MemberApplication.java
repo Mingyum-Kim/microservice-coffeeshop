43b1c016f88c7ca38a1963960e639980416d962f
MingyeomKim <67851124+MingyeomKim@users.noreply.github.com>
2023-03-28 22:13:52 +0900

    [hotfix] Config Server 설정 수정
    build.gradle 버전 수정

--- msa-service-coffee-member/src/main/java/com/personal/member/MemberApplication.java
+++ msa-service-coffee-member/src/main/java/com/personal/member/MemberApplication.java
@@ -2,8 +2,10 @@ package com.personal.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 public class MemberApplication {
 
     public static void main(String[] args) {
